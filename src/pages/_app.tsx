5d893e8b2247af2a78305c3e60fe4a656dc670c9
Bruno@Eduardo <bruno.3duardo@hotmail.com>
2022-03-25 23:50:13 -0300

    new test gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import { AppProps } from 'next/app';
-
+import * as gtag from '../lib/gtag';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
 import SidebarDrawerProvider from '../context/SidebarDrawerContext';
@@ -10,6 +10,7 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { hotjar } from 'react-hotjar';
 import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 if (process.env.NODE_ENV === 'development') {
 	makeServer();
@@ -18,11 +19,23 @@ if (process.env.NODE_ENV === 'development') {
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-	const HJID = 2894769
-	const HJSV = 6
+	const router = useRouter()
+
+	const HJID = 2894769;
+	const HJSV = 6;
 	useEffect(() => {
 		hotjar.initialize(HJID, HJSV);
 	}, []);
+
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			gtag.pageview(url);
+		};
+		router.events.on('routeChangeComplete', handleRouteChange);
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+		};
+	}, [router.events]);
 	return (
 		<QueryClientProvider client={queryClient}>
 			<ReactQueryDevtools />
