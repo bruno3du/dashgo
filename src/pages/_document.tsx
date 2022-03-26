8991ea258118f157bd6be800fba58d15a280030c
Bruno@Eduardo <bruno.3duardo@hotmail.com>
2022-03-25 23:42:43 -0300

    script added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,16 @@ export default class MyDocument extends Documents {
 						href='https://fonts.gstatic.com'
 						crossOrigin='true'
 					/>
-
+					<link href='https://www.googletagmanager.com/gtag/js?id=G-HFMS50QZ2H'></link>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							gtag('config', 'G-HFMS50QZ2H');
+							`,
+						}}></script>
 					<link
 						href='https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Roboto:ital,wght@0,400;0,700;1,400&display=swap'
 						rel='stylesheet'
